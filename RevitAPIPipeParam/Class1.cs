2cf3c6f8d9b941a06ec1806cccd6fc98d47605b5
Skitt2 <provaid@yandex.ru>
2022-05-05 23:33:33 +0300

    Add project files.

---
+++ RevitAPIPipeParam/Class1.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIPipeParam
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Application application = uiapp.Application;
+
+            List<Pipe> pipes = new FilteredElementCollector(doc)
+            .OfClass(typeof(Pipe))
+            .Cast<Pipe>()
+            .ToList();
+
+            foreach (var pipe in pipes)
+            {
+                string innerDiam = UnitUtils.ConvertFromInternalUnits(pipe.get_Parameter(BuiltInParameter.RBS_PIPE_INNER_DIAM_PARAM).AsDouble(), UnitTypeId.Millimeters).ToString();
+                string outerDiam = UnitUtils.ConvertFromInternalUnits(pipe.get_Parameter(BuiltInParameter.RBS_PIPE_OUTER_DIAMETER).AsDouble(), UnitTypeId.Millimeters).ToString();
+
+                using (Transaction ts = new Transaction(doc, "Set parameter"))
+                {
+                    ts.Start();
+
+                    Parameter NameParameter = pipe.LookupParameter("Наименование");
+                    NameParameter.Set($"Труба {innerDiam}/{outerDiam}");
+
+                    ts.Commit();
+                }
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
